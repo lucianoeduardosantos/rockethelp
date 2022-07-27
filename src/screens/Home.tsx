53f1f5ab722386881126c717569272b5eebd6243
Luciano Braga <lucianoeduardosantos@gmail.com>
2022-07-27 11:39:58 -0300

    AppV1

---
+++ src/screens/Home.tsx
@@ -0,0 +1,27 @@
+import { HStack, IconButton, useTheme, VStack } from 'native-base';
+import Logo from '../assets/logo_secondary.svg'
+import { SignOut } from 'phosphor-react-native';
+
+export function Home() {
+    const {colors} = useTheme();
+    return (
+        <VStack flex={1} pb={6} bg="gray.700">
+            <HStack
+                w="full"
+                justifyContent="space-between"
+                alignItems="center"
+                bg="gray.600"
+                pt={12}
+                pb={5}
+                px={6}
+            >
+                <Logo />
+                <IconButton 
+                    icon={<SignOut size={26} color={colors.gray[300]} />}
+                />
+
+            </HStack>
+
+        </VStack>
+    );
+}
